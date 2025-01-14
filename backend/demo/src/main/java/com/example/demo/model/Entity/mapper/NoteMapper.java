b25e39c44b0b722e6b2d2cb967d52b8abd4e57f0
GinoJSB <ginohable14@gmail.com>
2025-01-14 14:28:33 -0500

    Modify NoteMapper to support updated mapping logic for notes

--- backend/demo/src/main/java/com/example/demo/model/Entity/mapper/NoteMapper.java
+++ backend/demo/src/main/java/com/example/demo/model/Entity/mapper/NoteMapper.java
@@ -1,4 +1,15 @@
 package com.example.demo.model.Entity.mapper;
+import com.example.demo.model.DTO.NoteReqDTO;
+import com.example.demo.model.DTO.NoteResponseDTO;
+import com.example.demo.model.Entity.Note;
+import org.mapstruct.*;
 
+import java.util.List;
+
+@Mapper
 public interface NoteMapper {
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Note updateNoteReqDTOToNote(NoteReqDTO dto, @MappingTarget Note note);
+    NoteResponseDTO noteToNoteResponseDTO(Note note);
+    List<NoteResponseDTO> notesToNotesResponseDTO(List<Note> notes);
 }
